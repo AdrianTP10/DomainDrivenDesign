601b5c4423ee69bc431524ff78c994ac0a9316a2
Irving Torres <49598744+AdrianTP10@users.noreply.github.com>
2025-02-05 22:42:13 -0600

    added Jwt dependencies

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/VehiculoId.cs
@@ -0,0 +1,8 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace CleanArchitecture.Domain.Vehiculos;
+
+public record VehiculoId(Guid Value){
+    public static VehiculoId New() =>new (Guid.NewGuid());
+    
+}
